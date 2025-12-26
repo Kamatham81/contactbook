44e5e5d1b9047ae083339baef010ab0d6608576e
Kamatham81 <pujithakuruva52@gmail.com>
2025-12-26 12:46:29 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+const monogoose=require("mongoose")
+const connectDB=async()=>{
+    try{
+      await monogoose.connect("mongodb://127.0.0.1:27017/contact-manager")
+      console.log("DB CONNECTED")
+    }catch(err){
+        console.log("DB ERROR",err);
+    }
+}
+module.exports=connectDB;
