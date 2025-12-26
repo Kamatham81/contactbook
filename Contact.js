e0d9be95fd1e546aced21c9b263be7b25e223c0a
Kamatham81 <pujithakuruva52@gmail.com>
2025-12-26 12:48:56 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,7 @@
+const mongoose=require("mongoose")
+const ContactSchema=new mongoose.Schema({
+    name:String,
+    phone:String,
+    email:String,
+});
+module.exports=mongoose.model("Contact",ContactSchema);
